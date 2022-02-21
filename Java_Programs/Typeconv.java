75f641beb4e0b5541f5f6e549b6301a899c6eaa9
Madipadige-ManishKumar <oomanish459@gmail.com>
2022-02-21 11:46:46 +0530

    Added files: typec.java, typec.txt, Typeconv.java

---
+++ Java_Programs/Typeconv.java
@@ -0,0 +1,14 @@
+class Typeconv
+{
+	public static void main(String s[])
+	{
+		int a,b;
+		float c,d;
+		a=7;
+		d=2.718f;
+		c=a;
+		b=(int)d;
+		System.out.println("c="+c);
+		System.out.println("b="+b);
+	}
+}
---
+++ Java_Programs/typec.java
@@ -0,0 +1,14 @@
+class Typec
+{
+	public static void main(String s[])
+	{
+		int a,b;
+		float c,d;
+		a=7;
+		d=2.718;
+		c=a;
+		b=(int)d;
+		System.out.println("c="+c);
+		System.out.println("b="+b);
+	}
+}
