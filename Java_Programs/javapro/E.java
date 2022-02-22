e27fb6e7fb4137ece6942aafe30b25c3eb876aef
Madipadige-ManishKumar <oomanish459@gmail.com>
2022-02-22 20:37:07 +0530

    Added files: demo.java, E.java, F.java

---
+++ Java_Programs/javapro/E.java
@@ -0,0 +1,13 @@
+package p2;
+import p1.JavaExam;
+public class E	extends JavaExam
+{
+	public static void main(String []args)
+	{
+		JavaExam o = new JavaExam();
+		System.out.println("A  =  "+o.a);
+	//	System.out.println("B  =  "+o.b);	
+		System.out.println("C  =  "+o.c);
+	//	System.out.println("D  =  "+o.d);
+	}
+}
---
+++ Java_Programs/javapro/F.java
@@ -0,0 +1,15 @@
+package pak1.pack2;
+public class F
+{
+	public static  int a=10;
+	static int b=20;
+	protected static  int c=30;
+	private   static int d=40;
+	public static void main(String []args)
+	{
+		System.out.println("A  =  "+a);
+		System.out.println("B  =  "+b);	
+		System.out.println("C  =  "+c);
+		System.out.println("D  =  "+d);
+	}
+}
