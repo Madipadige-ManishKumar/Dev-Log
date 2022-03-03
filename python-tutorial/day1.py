183fe0ab0570dfb91ba1e0e038cbce685d03d65b
Madipadige-ManishKumar <oomanish459@gmail.com>
2022-03-03 11:56:45 +0530

    Added files: day1.py, rough.py, aggresive_cows.html

---
+++ python-tutorial/day1.py
@@ -0,0 +1,85 @@
+
+# # print("Welcome to Lunocode")
+
+
+# # Single    comment 
+
+# '''
+# Multi -line comment 
+# '''
+# # variable 
+# '''
+# syntax:
+# Variable _Name =Value 
+# '''
+# x=7
+# print(x)
+# # print(X) # case - sensitive 
+
+# #camel case 
+# myVar="VAlUE"
+# #snake case 
+# my_var="VAL"
+# print(myVar)
+# print(my_var)
+
+
+# # data types 
+# name="Luno"  # string 
+# age=25 # integer 
+# marks = 85.40  # float 
+
+# print(f"Name:{name} Age:{age} marks:{marks}")
+
+# # type()
+
+# print(f"Name_type:{type(name)}  Age_type:{type(age)}  marks_type:{type(marks)}")
+
+# #list
+# l=[1,2,3,4]
+# print(l[-1])
+# # methods of list 
+# l.append(5)
+# print(l)
+# l.insert(0,0)
+# print(l)
+# l.extend([6,7,8])
+# print(l)
+# l.pop()
+# print(l)
+# l.pop(3)  # index 
+# print(l)
+# l.remove(4)  # value 
+# print(l)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# print("tuple")
+
+# mytuple=(1,2,3,4,1)
+# print(mytuple)
+# print(type(mytuple))
+# print("The No. of 1 in tuple ",mytuple.count(2))
+# print("The index of value 1 is ",mytuple.index(1))
+
+
+# print("sets")
+# myset={1,2,3,4,1}
+# print(type((myset)))
+# print(myset)
+# #set methods 
+# myset.add(5)
+# print(myset)
+# print(myset.pop())
+# print(myset)
+
+#Dictionary
+
+my_dict={
+    "name":"Luno",
+    "age":25,
+    "marks":84.5
+}
+
+print(my_dict["age"])
