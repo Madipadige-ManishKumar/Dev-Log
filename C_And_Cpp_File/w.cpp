24f0f9908a770c5fe84f55d71c4a40d1476b5575
Madipadige-ManishKumar <oomanish459@gmail.com>
2022-10-21 08:51:31 +0530

    Added files: Untitled1.cpp, w.cpp, work.cpp

---
+++ C_And_Cpp_File/w.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	int i=0;
+	while(i=1){
+	
+	printf("True");
+}
+	printf("False");
+	return 0;
+}
---
+++ C_And_Cpp_File/work.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+;
+char a='a';
+printf("%i",a);
+	return 0;
+}
